d478c0985bf126d75e8eb3501723bb26f929e6b1
Saul van der Walt <saulvdw@gmail.com>
2018-11-14 06:51:18 +0200

    serve from cache

--- backend/admin.go
+++ backend/admin.go
@@ -2,6 +2,9 @@ package backend
 
 func initAdmin() {
 	Server.GET("/admin", AdminHandle(func(c ctx, user *User) error {
+		if Cache != nil {
+			return Cache.ServeFile(c.Response(), c.Request(), "/admin.html")
+		}
 		return c.File("./assets/admin.html")
 	}))
 }
