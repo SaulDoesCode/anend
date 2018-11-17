0e131a192268904fdb131aced344d8960d1e5f68
Saul van der Walt <saulvdw@gmail.com>
2018-11-17 16:40:44 +0200

    dangerous but very useful/extensible, expose /query for admins

--- backend/admin.go
+++ backend/admin.go
@@ -7,4 +7,23 @@ func initAdmin() {
 		}
 		return c.File("./assets/admin.html")
 	}))
+
+	Server.POST("/query", AdminHandle(func(c ctx, user *User) error {
+		var vars obj
+		err := c.Bind(&vars)
+		if err != nil {
+			return c.Msgpack(400, obj{"err": err.Error()})
+		}
+		query, ok := vars["query"].(string)
+		if !ok {
+			return c.Msgpack(400, obj{"err": "cannot query without an actual query to query with"})
+		}
+		delete(vars, "query")
+
+		out, err := Query(query, vars)
+		if err != nil {
+			return c.Msgpack(500, obj{"err": err.Error()})
+		}
+		return c.Msgpack(200, out)
+	}))
 }
